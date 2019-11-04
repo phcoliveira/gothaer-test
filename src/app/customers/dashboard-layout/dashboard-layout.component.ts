13a3ce328f27e2085eff5e7f75ec66269568dc0a
Paulo Oliveira <oliveira.phc@gmail.com>
2019-11-04 10:40:18 +0000

    Removed unnecessary comments

--- src/app/customers/dashboard-layout/dashboard-layout.component.ts
+++ src/app/customers/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
 import { AngularFireAuth } from '@angular/fire/auth'
 
-// import { Subject } from 'rxjs'
-// import { takeUntil } from 'rxjs/operators'
-
-// import { CustomerService } from '../customer.service'
 import { Customer } from '../customer.model'
 
 @Component({
@@ -13,13 +9,10 @@ import { Customer } from '../customer.model'
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.scss'],
 })
-export class DashboardLayoutComponent implements OnInit, OnDestroy {
+export class DashboardLayoutComponent implements OnInit {
   public customer: Customer
 
-  // private subscriptions$: Subject<void> = new Subject<void>()
-
   constructor(
-    // private heroService: CustomerService,
     private fireauth: AngularFireAuth,
     private route: ActivatedRoute,
     private router: Router,
@@ -29,16 +22,6 @@ export class DashboardLayoutComponent implements OnInit, OnDestroy {
     const { customer } = this.route.snapshot.data
 
     this.customer = customer
-    // this.heroService.customer$
-    //   .pipe(takeUntil(this.subscriptions$))
-    //   .subscribe((customer: Customer) => {
-    //     this.customer = customer
-    //   })
-  }
-
-  ngOnDestroy(): void {
-    // this.subscriptions$.next()
-    // this.subscriptions$.complete()
   }
 
   signOut(): void {
